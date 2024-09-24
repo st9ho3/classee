14811c4cd836fb44205f223516e8e8cf897be7f8
st9ho3 <stahos21@gmail.com>
2024-09-24 20:47:01 +0300

    auth set

--- src/components/class/Classes.jsx
+++ src/components/class/Classes.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { localStorageCall } from '../../utils/LocalStorageCall'
 
 const Classes = () => {
-  const saved = localStorageCall('Classes')
+  const user = localStorageCall('auth_user')
+  const classes = user?.Classes || [] // Use optional chaining to handle undefined
+  console.log(classes)
+
   return (
     <div className='classes'>
-      {saved.map((sav) => (
-        <div key={sav.classId} className="class">{sav.name}</div>
+      {classes.map(classItem => (
+        <div key={classItem.classId}>{classItem.name}</div>
       ))}
     </div>
   )
