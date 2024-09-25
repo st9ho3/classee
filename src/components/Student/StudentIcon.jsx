3e0191fd25bb2a969a3f96b6736617e6a71de024
st9ho3 <stahos21@gmail.com>
2024-09-25 21:38:43 +0300

    stdents store

--- src/components/Student/StudentIcon.jsx
+++ src/components/Student/StudentIcon.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const StudentIcon = ({type}) => {
+  const randomNumber = Math.ceil(Math.random()*7)
+  console.log(randomNumber)
   return (
     <div>
-      <img className={type === 'store' ? 'studentIcon storeIcon' : 'studentIcon'} src="vite.svg" alt="student-icon" />
+      <img className={type === 'store' ? 'studentIcon storeIcon' : 'studentIcon'} src={`boy`+randomNumber+`.png`} alt="student-icon" />
     </div>
   )
 }
