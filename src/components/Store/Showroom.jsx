0100ef0edaeaa071a4dbfc95cfd7323d40e454d9
st9ho3 <stahos21@gmail.com>
2024-09-27 15:24:05 +0300

    stuff

--- src/components/Store/Showroom.jsx
+++ src/components/Store/Showroom.jsx
@@ -4,7 +4,7 @@ const Showroom = ({children,goods, type}) => {
   return (
     <div
      className={type === 'bigger' ? 'showroom bigger' : 'showroom'}
-     style={goods.length <= 15 ? {overflowY: 'hidden'} : {overflowY: 'scroll'}} >
+     style={type === 'bigger' ? goods.length <= 12 ? {overflowY: 'hidden'} : {overflowY: 'scroll'}: goods.length <= 15 ? {overflowY: 'hidden'} : {overflow: 'scroll'}} >
       {children}
     </div>
   )
